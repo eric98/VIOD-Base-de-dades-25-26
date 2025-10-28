786b513eb9818e7cd05d634c2e1fea5b5206e3a0
eric-viod <ergare.17@gmail.com>
2025-10-28 14:01:59 +0100

    aa2p1e2 en procés

---
+++ aa2p1e4.sql
@@ -0,0 +1,27 @@
+CREATE TABLE empleat (
+	ID_E INT PRIMARY KEY,
+	DNI_E CHAR(9),
+	Nom VARCHAR(20),
+	Salari INT
+	Codi_localitat INT,
+	FOREIGN KEY (Codi_localitat) REFERENCES localitat(Codi)
+);
+
+CREATE TABLE localitat (
+	Codi INT PRIMARY KEY,
+	Nom VARCHAR(20),
+	Codi_provincia INT,
+	FOREIGN KEY (Codi_provincia) REFERENCES provincia(Codi)
+);
+
+CREATE TABLE provincia (
+	Codi INT PRIMARY KEY,
+	Nom VARCHAR(20),
+	Codi_regio INT,
+	FOREIGN KEY (Codi_regio) REFERENCES regio(Codi)
+);
+
+CREATE TABLE regio (
+	Codi INT PRIMARY KEY,
+	Nom VARCHAR(20)
+);
